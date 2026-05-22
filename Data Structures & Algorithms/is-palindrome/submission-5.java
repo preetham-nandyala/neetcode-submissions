09bc0cf47a34db205a9543a244e009f1f983a9eb
Preetham <nandyala.preetham755@gmail.com>
2026-05-22 23:14:14 +0530

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.java
@@ -0,0 +1,6 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]","");
+        return s.equals(new StringBuilder(s).reverse().toString());
+    }
+}
